8cb9d58ec392a92db085cda0f5204acaaa0c4010
EnTeeGee <enteegee@protonmail.com>
2019-12-02 20:10:51 +0000

    Solutions for day 2

--- AdventOfCode2019/Common/Mapper.cs
+++ AdventOfCode2019/Common/Mapper.cs
@@ -10,9 +10,19 @@ namespace AdventOfCode2019.Common
             return input.Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
         }
 
+        public static string[] ToCsvs(string input)
+        {
+            return input.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+
         public static int[] ToInts(string input)
         {
             return ToLines(input).Select(it => Convert.ToInt32(it)).ToArray();
+        }
+
+        public static int[] ToCsvInts(string input)
+        {
+            return ToCsvs(input).Select(it => Convert.ToInt32(it)).ToArray();
         }
     }
 }
---
+++ AdventOfCode2019/Solutions/Day02.cs
@@ -0,0 +1,93 @@
+﻿using AdventOfCode2019.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2019.Solutions
+{
+    class Day02
+    {
+        [Solution(2, 1)]
+        public string Problem1(string input)
+        {
+            var program = Mapper.ToCsvInts(input);
+            var index = 0;
+            program[1] = 12;
+            program[2] = 2;
+
+            while(true)
+            {
+                switch(program[index])
+                {
+                    case 99:
+                        return program[0].ToString();
+                    case 1:
+                        program[program[index + 3]] = program[program[index + 1]] + program[program[index + 2]];
+                        break;
+                    case 2:
+                        program[program[index + 3]] = program[program[index + 1]] * program[program[index + 2]];
+                        break;
+                    default:
+                        Console.WriteLine("Error:");
+                        Console.WriteLine(index);
+                        Console.WriteLine(String.Join(",", program));
+                        throw new Exception("Unexpected opcode");
+                }
+
+                index += 4;
+            }
+        }
+
+        // Ha ha, time for brute force
+        [Solution(2, 2)]
+        public string Problem2(string input)
+        {
+            var parsed = Mapper.ToCsvInts(input);
+            var target = "19690720";
+
+            for (var i = 0; i < 100; i++)
+            {
+                for(var j = 0; j < 100; j++)
+                {
+                    var result = RunWithValues(parsed.ToArray(), i, j);
+
+                    if (result == target)
+                        return ((100 * i) + j).ToString();
+                }
+            }
+
+            throw new Exception("No working pair found");
+        }
+
+        private string RunWithValues(int[] input, int noun, int verb)
+        {
+            var index = 0;
+            input[1] = noun;
+            input[2] = verb;
+
+            while (true)
+            {
+                switch (input[index])
+                {
+                    case 99:
+                        return input[0].ToString();
+                    case 1:
+                        input[input[index + 3]] = input[input[index + 1]] + input[input[index + 2]];
+                        break;
+                    case 2:
+                        input[input[index + 3]] = input[input[index + 1]] * input[input[index + 2]];
+                        break;
+                    default:
+                        Console.WriteLine("Error:");
+                        Console.WriteLine(index);
+                        Console.WriteLine(String.Join(",", input));
+                        throw new Exception("Unexpected opcode");
+                }
+
+                index += 4;
+            }
+        }
+    }
+}
